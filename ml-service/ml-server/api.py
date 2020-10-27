d2bbe0dd081b89e4a3279676adba44481c351c89
tunveyyy <panditanvi@gmail.com>
2020-10-26 20:56:43 -0400

    Team assigner working

--- ml-service/ml-server/api.py
+++ ml-service/ml-server/api.py
@@ -1,15 +1,13 @@
 # import time
 from flask import Flask
-from TeamAssigner import assignTeam
+from .TeamAssigner import assignTeam
 
 app = Flask(__name__)
 
 
-@app.route('/executeAlgo')
+@app.route("/executeAlgo")
 def execute_algo():
-    exec(open("algo.py").read())
-    # exec(open("algo.py").read())
-    assignTeam()	
+    assignTeam()
     return {
-        'msg': 'success',
+        "msg": "success",
     }
