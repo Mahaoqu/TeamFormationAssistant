b918a27c546177470ab53c88a02f4b2a766a20d5
Amit Mandliya <amitmandliya.davviet@gmail.com>
2020-10-26 21:20:48 -0400

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
