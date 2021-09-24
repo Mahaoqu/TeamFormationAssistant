407205980ec13c7d6ab84247e4133c10e72b578b
hanseglz <glzhang23@gmail.com>
2021-09-24 18:01:50 -0400

    update home page

--- frontend/view/src/components/home.component.js
+++ frontend/view/src/components/home.component.js
@@ -1,16 +1,47 @@
 import React, { Component } from "react";
-
+import AuthService from "../services/auth.service"
 import UserService from "../services/user.service";
 
 export default class Home extends Component {
-  
-  render() {
-    return (
-     
-     
-          <h3>7893</h3>
-     
 
+  state = {
+    hour:null,
+    username:'Hans'
+  }
+
+  componentDidMount() {
+    this.getHour()
+    this.getUser()
+  }
+
+  getHour = () => {
+    const hour = this.getTimePhase()
+    this.setState({hour})
+  }
+
+  getTimePhase = () => {
+    const date = new Date()
+    const hour = date.getHours()
+    if (hour < 12){
+      return "morning"
+    } else if (hour < 18){
+      return "afternoon"
+    } else {
+      return "evening"
+    }
+  }
+
+  getUser = () => {
+    const username = AuthService.getCurrentUser().username
+    this.setState({username})
+  }
+
+  render() {
+    const {hour, username} = this.state;
+    return (
+        <div className='App'>
+          <h3>{`Good ${hour}, ${username}!`}</h3>
+        </div>
     );
   }
 }
