8774d7066c00412232bdb3aef1a85661430efad7
hanseglz <glzhang23@gmail.com>
2021-09-25 00:43:35 -0400

    Header.js reformat

--- frontend/view/src/components/Header.js
+++ frontend/view/src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import React, {Component} from "react";
+import {Switch, Route, Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import AuthService from "../services/auth.service";
@@ -22,152 +22,149 @@ import Projectinc from "./projectinc";
 
 class Header extends Component {
     constructor(props) {
-      super(props);
-      this.logOut = this.logOut.bind(this);
-  
-      this.state = {
-        showManagerBoard: false,
-        showAdminBoard: false,
-        currentUser: undefined,
-      };
+        super(props);
+        this.logOut = this.logOut.bind(this);
+
+        this.state = {
+            showManagerBoard: false,
+            showAdminBoard: false,
+            currentUser: undefined,
+        };
     }
-  
+
     componentDidMount() {
-      const user = AuthService.getCurrentUser();
-  
-      if (user) {
-        this.setState({
-          currentUser: user,
-          showManagerBoard: user.roles.includes("ROLE_MANAGER") || user.roles.includes("ROLE_ADMIN"),
-          showAdminBoard: user.roles.includes("ROLE_ADMIN"),
-  
+        const user = AuthService.getCurrentUser();
+
+        if (user) {
+            this.setState({
+                currentUser: user,
+                showManagerBoard: user.roles.includes("ROLE_MANAGER") || user.roles.includes("ROLE_ADMIN"),
+                showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+
+            });
+        }
+
+        EventBus.on("logout", () => {
+            this.logOut();
         });
-      }
-      
-      EventBus.on("logout", () => {
-        this.logOut();
-      });
     }
-  
+
     componentWillUnmount() {
-      EventBus.remove("logout");
+        EventBus.remove("logout");
     }
-  
+
     logOut() {
-      AuthService.logout();
-      this.setState({
-        showManagerBoard: false,
-        showAdminBoard: false,
-        currentUser: undefined,
-      });
+        AuthService.logout();
+        this.setState({
+            showManagerBoard: false,
+            showAdminBoard: false,
+            currentUser: undefined,
+        });
     }
-  
+
     render() {
-      const { currentUser, showManagerBoard, showAdminBoard } = this.state;
-  
-      return (
-        <div>
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="/">Teamformation Assistant</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="me-auto">
-      <Nav.Link href="/home">home</Nav.Link>
-      {showManagerBoard && (
-        <NavDropdown title="project" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="/teamMatch">team match</NavDropdown.Item>
-          <NavDropdown.Item href="/proj_inc">project management</NavDropdown.Item>
-        </NavDropdown>
-        )}
-  
-      {showManagerBoard && (
-        <NavDropdown title="recruit" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="/jobMatch">candidate match</NavDropdown.Item>
-            <NavDropdown.Item href="/job_inc">job management</NavDropdown.Item>
-        </NavDropdown>
-      )}
-  
-  {showAdminBoard && (  
-        <NavDropdown title="employee" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="/admin">employee management</NavDropdown.Item>
-        </NavDropdown>
-  )}
-     {currentUser && (<NavDropdown title="application" id="collasible-nav-dropdown">
-       
-       {!showManagerBoard && ( 
-      <fragment>
-          <NavDropdown.Item href="/user">project application</NavDropdown.Item>
-          <NavDropdown.Item href="/user">job application</NavDropdown.Item>
-          <NavDropdown.Item href="/user">my application</NavDropdown.Item>
-      </fragment>
-  )}
-      {showManagerBoard && (  
-        <fragment>
-        <NavDropdown.Item href="/man">application management</NavDropdown.Item> 
-        </fragment>
-        )}
-      </NavDropdown>)}
-  
-  
-  
-  
-  
-      </Nav>
-      <Nav>
-      {currentUser ? (
-              <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to={"/profile"} className="nav-link">
-                    {currentUser.username}
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <a href="/login" className="nav-link" onClick={this.logOut}>
-                    LogOut
-                  </a>
-                </li>
-              </div>
-            ) : (
-              <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to={"/login"} className="nav-link">
-                    Login
-                  </Link>
-                </li>
-  
-                <li className="nav-item">
-                  <Link to={"/register"} className="nav-link">
-                    Sign Up
-                  </Link>
-                </li>
-              </div>
-            )}
-      </Nav>
-    </Navbar.Collapse>
-    </Container>
-  </Navbar>
-         
-  
-          <div className="container mt-3">
-            <Switch>
-              <Route exact path={["/", "/home"]} component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/profile" component={Profile} />
-              <Route path="/user" component={BoardUser} />
-              <Route path="/teamMatch" component={TeamMatch} />
-              <Route path="/proj_inc" component={Projectinc} />
-              <Route path="/admin" component={BoardAdmin} />
-              <Route path="/teamMatch" Component={TeamMatch}/>
-            </Switch>
-           
-          </div>
-  
-          { /*<AuthVerify logOut={this.logOut}/> */ }
-        </div>
-      );
+        const {currentUser, showManagerBoard, showAdminBoard} = this.state;
+
+        return (
+            <div>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="/">Teamformation Assistant</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="/home">home</Nav.Link>
+                                {showManagerBoard && (
+                                    <NavDropdown title="project" id="collasible-nav-dropdown">
+                                        <NavDropdown.Item href="/teamMatch">team match</NavDropdown.Item>
+                                        <NavDropdown.Item href="/proj_inc">project management</NavDropdown.Item>
+                                    </NavDropdown>
+                                )}
+
+                                {showManagerBoard && (
+                                    <NavDropdown title="recruit" id="collasible-nav-dropdown">
+                                        <NavDropdown.Item href="/jobMatch">candidate match</NavDropdown.Item>
+                                        <NavDropdown.Item href="/job_inc">job management</NavDropdown.Item>
+                                    </NavDropdown>
+                                )}
+
+                                {showAdminBoard && (
+                                    <NavDropdown title="employee" id="collasible-nav-dropdown">
+                                        <NavDropdown.Item href="/admin">employee management</NavDropdown.Item>
+                                    </NavDropdown>
+                                )}
+                                {currentUser && (<NavDropdown title="application" id="collasible-nav-dropdown">
+
+                                    {!showManagerBoard && (
+                                        <fragment>
+                                            <NavDropdown.Item href="/user">project application</NavDropdown.Item>
+                                            <NavDropdown.Item href="/user">job application</NavDropdown.Item>
+                                            <NavDropdown.Item href="/user">my application</NavDropdown.Item>
+                                        </fragment>
+                                    )}
+                                    {showManagerBoard && (
+                                        <fragment>
+                                            <NavDropdown.Item href="/man">application management</NavDropdown.Item>
+                                        </fragment>
+                                    )}
+                                </NavDropdown>)}
+
+
+                            </Nav>
+                            <Nav>
+                                {currentUser ? (
+                                    <div className="navbar-nav ml-auto">
+                                        <li className="nav-item">
+                                            <Link to={"/profile"} className="nav-link">
+                                                {currentUser.username}
+                                            </Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a href="/login" className="nav-link" onClick={this.logOut}>
+                                                LogOut
+                                            </a>
+                                        </li>
+                                    </div>
+                                ) : (
+                                    <div className="navbar-nav ml-auto">
+                                        <li className="nav-item">
+                                            <Link to={"/login"} className="nav-link">
+                                                Login
+                                            </Link>
+                                        </li>
+
+                                        <li className="nav-item">
+                                            <Link to={"/register"} className="nav-link">
+                                                Sign Up
+                                            </Link>
+                                        </li>
+                                    </div>
+                                )}
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+
+
+                <div className="container mt-3">
+                    <Switch>
+                        <Route exact path={["/", "/home"]} component={Home}/>
+                        <Route exact path="/login" component={Login}/>
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/profile" component={Profile}/>
+                        <Route path="/user" component={BoardUser}/>
+                        <Route path="/teamMatch" component={TeamMatch}/>
+                        <Route path="/proj_inc" component={Projectinc}/>
+                        <Route path="/admin" component={BoardAdmin}/>
+                        <Route path="/teamMatch" Component={TeamMatch}/>
+                    </Switch>
+
+                </div>
+
+                { /*<AuthVerify logOut={this.logOut}/> */}
+            </div>
+        );
     }
-  }
+}
 
 export default Header;
