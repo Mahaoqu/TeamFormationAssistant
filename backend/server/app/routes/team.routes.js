00239b1e9c617a91ebbe570cb360ad56f72fb596
hanseglz <glzhang23@gmail.com>
2021-09-25 15:57:08 -0400

    update db.config.js & reformat

--- backend/server/app/routes/team.routes.js
+++ backend/server/app/routes/team.routes.js
@@ -4,18 +4,18 @@ const mysql = require('mysql2');
 
 // create a connection variable
 const con = mysql.createConnection({
-  host: 'database', // server ip address
-  port: '3306',
-  user: 'dbuser', // user name
-  password: 'dbuserpwd', // password
-  database: 'teamformationassistant', // database name
+    host: 'database', // server ip address
+    port: '3306',
+    user: 'dbuser', // user name
+    password: 'dbuserpwd', // password
+    database: 'teamformationassistant', // database name
 });
 
 // connect to the database.
 con.connect((err) => {
-  if (err) throw err;
-  // if connection successful
-  console.log('connection successful!!!');
+    if (err) throw err;
+    // if connection successful
+    console.log('connection successful!!!');
 });
 router.get('/', (req, res) => {
     con.query('SELECT * from Team', (err, rows, fields) => {
