208f5ec11fda824c24f2a01ad4eede460e351053
hanseglz <glzhang23@gmail.com>
2021-09-30 23:29:07 -0400

    fix testing

--- backend/server/test/test_api.py
+++ backend/server/test/test_api.py
@@ -9,10 +9,8 @@ import requests
 sys.path.append(".")
 # from test.connection import connect
 # from test import connection
-from backend.server.test.connection import connect
-from backend.server.test import connection
-# from server.test.connection import connect
-# from server.test import connection
+from server.test.connection import connect
+from server.test import connection
 
 TEST_DATA_JOB = {
     "JobName": "testscript",
