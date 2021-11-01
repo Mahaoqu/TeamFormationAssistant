0f9183ebf935594b07a063755a148ea7653094a4
zhijin44 <zhijinyang@outlook.com>
2021-11-01 15:26:05 -0400

    add candidatesService

--- backend/src/candidates/candidates.service.ts
+++ backend/src/candidates/candidates.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCandidateDto } from './dto/create-candidate.dto';
 import { UpdateCandidateDto } from './dto/update-candidate.dto';
+import { Project } from 'src/projects/entities/project.entity';
+import { Application } from 'src/applications/entities/application.entity';
+import { Candidate } from './entities/candidate.entity';
+import { Requirement } from 'src/projects/entities/requirement.entity';
 
 @Injectable()
 export class CandidatesService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+
+    @InjectRepository(Application)
+    private readonly applicationRepository: Repository<Application>,
+
+    @InjectRepository(Candidate)
+    private readonly candidateRepository: Repository<Candidate>,
+
+    @InjectRepository(Requirement)
+    private readonly requirementRepository: Repository<Requirement>,
+  ) {}
+  
+  async assign() {
+    const requirementsIDs = await this.requirementRepository.find();
+    const employee = await this.applicationRepository.find();
+    // for (const proj of projs) {
+    //   for (const r of proj.requirements) {
+        
+    //   }
+    // }
+
+    const c = new Candidate();
+    // c.project = 
+    await this.candidateRepository.save(c);
+  }
+
   create(createCandidateDto: CreateCandidateDto) {
     return 'This action adds a new candidate';
   }
